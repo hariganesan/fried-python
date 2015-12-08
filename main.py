afbfa69531ef4d1ead328ddd5ff7490441a02654
hariganesan <hari@getbriefme.com>
2015-12-08 00:14:01 -0500

    More byes

--- main.py
+++ main.py
@@ -6,36 +6,37 @@ import os
 from google.appengine.ext.webapp import template
 import webapp2
 
-# spits a path to a request handler
-def spitPath(self, path):
-    fullPath = os.path.join(os.path.dirname(__file__), path)
-    self.response.out.write(template.render(fullPath, {}))
-
-
-#########################
-# Static Handlers
-#########################
 
 # main routes to static pages
 class MainPageHandler(webapp2.RequestHandler):
     def get(self):
         spitPath(self, "templates/index.html")
 
+
 class PortfolioPageHandler(webapp2.RequestHandler):
     def get(self):
         spitPath(self, "templates/portfolio.html")
+
 
 class AboutPageHandler(webapp2.RequestHandler):
     def get(self):
         spitPath(self, "templates/about.html")
 
+
 class ColorsPageHandler(webapp2.RequestHandler):
     def get(self):
         spitPath(self, "templates/colors.html")
 
+
 class NotFoundPageHandler(webapp2.RequestHandler):
     def get(self):
         spitPath(self, "templates/notfound.html")
+
+
+# spits a path to a request handler
+def spitPath(self, path):
+    fullPath = os.path.join(os.path.dirname(__file__), path)
+    self.response.out.write(template.render(fullPath, {}))
 
 
 app = webapp2.WSGIApplication([
